3422197c25f4e2bf245908751c7eb6fc6cbb519f
katapy <katapy.tennis@gmail.com>
2022-03-20 22:27:31 +0900

    Set circle ci

--- src/manager/data/definition.rs
+++ src/manager/data/definition.rs
@@ -14,13 +14,17 @@ pub mod definition {
     }
 
     impl Child {
+        /// get multi type from u8 num.
         pub fn get_multi_type_u8_num(&self) -> Result<u8> {
             match self.multi_type {
                 MultiType::Single => Ok(0x00),
                 _ => Err("Multi type convert error")?
             }
         }
-
+        
+        /// create child.
+        /// * `tag` - Child tag.
+        /// * `tag` - Child multi type.
         pub fn create(tag: u32, multu_type: MultiType) -> Child {
             Child {
                 tag: tag,
@@ -28,6 +32,9 @@ pub mod definition {
             }
         }
 
+        /// create child by binary data.
+        /// * `tag` - Child tag.
+        /// * `tag` - Child multi type which binary.
         pub fn create_by_binary(tag: u32, multi_type_binnary: u8) -> Child {
             let multi_type = match multi_type_binnary {
                 0x00 => MultiType::Single,
